4789a362f1dfdcacbe0c3ccd91ab9489ea67677f
keranjan <gnn6rrzgxf@privaterelay.appleid.com>
2026-08-07 15:30:17 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * Kun muutat index.html:ää, nosta CACHE-versiota. Muuten selain voi tarjoilla
  * vanhaa runkoa vielä pitkään.
  */
-const CACHE = "maalausurakka-v3.2";
+const CACHE = "maalausurakka-v3.3";
 
 /* sovelluksen runko */
 const SHELL = [
